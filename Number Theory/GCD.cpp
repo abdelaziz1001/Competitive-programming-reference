14687d83dedc632975eb937cc6ef25d7cccce9f5
abdelaziz1001 <86232978+abdelaziz1001@users.noreply.github.com>
2021-09-19 13:18:26 +0200

    Create GCD.cpp

---
+++ Number Theory/GCD.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+
+    while(b!=0)
+    {
+        ll a2 = a;
+        a = b;
+        b = a2 % b;
+    }
+    return a;
+}
+
+// Recursive function
+int gcd_rec(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+
+int main()
+{
+
+    cout << gcd(24, 6) << "\n";
+    cout << gcd_rec(24, 6) << "\n";
+
+    return 0;
+}
